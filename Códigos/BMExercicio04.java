13991a1fa4f0f366b80c2c497eb935b991999386
Maurício Bernardo <mauriciobernardo.mbd@gmail.com>
2023-03-24 21:01:27 -0300

    Conclui atividade

---
+++ Códigos/BMExercicio04.java
@@ -0,0 +1,13 @@
+public class BMExercicio04 {
+
+    public static void main (String[] arg) {
+
+        long long1 = 100L;
+
+        long int1 = 200;
+
+        long int2 = long1 + int1;
+        
+    }
+
+}
